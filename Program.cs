3c88b429d8d068ce11effbcfd91d3731e4a8e8ea
KasparSiivelt5 <89830554+KasparSiivelt5@users.noreply.github.com>
2021-09-07 12:18:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace PermanentDrivingLicence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja sünniaastat
+            //programm kontrollib kasutaja vanust
+            //programm teatab kasutajat, kas ta on piisavalt vana
+            //et juhiluba saada
+
+            {
+                Console.WriteLine("Mis aastal sa oled sündinud?");
+                int YearOfBirth = Int32.Parse(Console.ReadLine());
+                int Age = 2021 - YearOfBirth;
+
+                if( Age < 18)
+                {
+                    Console.WriteLine("Sa oled piisavalt vana ,et juhiluba saada");
+
+                }
+                  else if(Age > 18 )          
+                {
+                    Console.WriteLine("Oled liiga noor ,et juhiluba saada");
+
+                }
+                  else
+                {
+                    Console.WriteLine("Oled 18 siis saad juhiloa");
+                }
+                
+                
+            
+            
+            
+            
+            
+            
+             }
+
+        }
+    }
+}
